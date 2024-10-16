15ec73158113b13f9b37cc590cc51230d8cfae98
Anton Alalaev <antonalalaev@gmail.com>
2024-10-16 17:40:18 +0500

    Added ComplReader class

---
+++ UnionCompl/ComplReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnionCompl
+{
+    internal class ComplReader
+    {
+        /// <summary>
+        /// Словарь с элементами комплектации
+        /// </summary>
+        public Dictionary<string, Dictionary<string, int>> Elements;
+
+        public string file_name;
+
+        public ComplReader()
+        {
+            Elements = new Dictionary<string, Dictionary<string, int>>();
+            file_name = "";
+        }
+
+        public ComplReader(string file_name)
+        {
+            Elements = new Dictionary<string, Dictionary<string, int>>();
+            this.file_name = file_name;
+        }
+
+    }
+}
